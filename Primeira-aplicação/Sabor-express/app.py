8b011c6743e95e07df932f6066b5b720cf4f97ef
Derek Barragão <42359941+derektsc@users.noreply.github.com>
2026-04-24 22:21:28 -0300

    Initial Commit app.py

---
+++ Primeira-aplicação/Sabor-express/app.py
@@ -0,0 +1,10 @@
+print ('Sabor Express')
+
+print ('1. Cadastrar restaurante')
+print ('2. Listar restaurante')
+print ('3. Ativar restaurante')
+print ('4. Sair\n')
+
+
+opcao_escolhida = input('Escolha uma Opção ')
+print(f'Voce escolheu a opção {opcao_escolhida}')
